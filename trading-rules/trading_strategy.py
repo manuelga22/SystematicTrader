bb0e8a06dc2bd082dde5f504ec29a061c85886ba
manuelgarcia_22 <manuelisaacgarcia@gmail.com>
2025-11-15 18:59:19 -0500

    creating TradingStrategy class

---
+++ trading-rules/trading_strategy.py
@@ -0,0 +1,17 @@
+
+
+class TradingStrategy:
+    def __init__(self):
+        pass
+
+    def generate_signal(self, market_data, positions_data):
+        """
+        Generate trading signals based on market data.
+
+        Parameters:
+        market_data (DataFrame): A DataFrame containing market data with columns such as 'price', 'volume', etc.
+
+        Returns:
+        signals (TradingSignal): An instance of TradingSignal indicating BUY, HOLD, or SELL.
+        """
+        pass
